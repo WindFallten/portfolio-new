240ba77e99457a9ce1275bb615a5ddf54853b0a2
WindFallten <egorsig@yandex.ru>
2024-12-26 02:50:27 +0300

    tytyty

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,54 +1,38 @@
-import Card from "../../components/card";
+import Link from "next/link";
+import Image from "next/image";
+
 
 export default function Portfolio() {
-    const portfolios = [
-        {
-            id: 1,
-            image: "https://via.placeholder.com/300x200",
-            title: "Портфолио 1",
-            description: "Описание первого портфолио.",
-        },
-        {
-            id: 2,
-            image: "https://via.placeholder.com/300x200",
-            title: "Портфолио 2",
-            description: "Описание второго портфолио.",
-        },
-        {
-            id: 3,
-            image: "https://via.placeholder.com/300x200",
-            title: "Портфолио 3",
-            description: "Описание третьего портфолио.",
-        },
-        {
-            id: 4,
-            image: "https://via.placeholder.com/300x200",
-            title: "Портфолио 4",
-            description: "Описание четвёртого портфолио.",
-        },
-        {
-            id: 5,
-            image: "https://via.placeholder.com/300x200",
-            title: "Портфолио 5",
-            description: "Описание пятого портфолио.",
-        },
-    ];
 
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100">
-            <div className="container mx-auto px-4 py-8">
-                <h1 className="text-4xl font-bold mb-8 text-center">Мои портфолио</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {portfolios.map((portfolio) => (
-                        <Card
-                            key={portfolio.id}
-                            id={portfolio.id}
-                            image={portfolio.image}
-                            title={portfolio.title}
-                            description={portfolio.description}
-                        />
-                    ))}
-                </div>
+        <div className="min-h-screen bg-background">
+            <h1 className="text-4xl font-bold mb-8 text-center">Мои портфолио</h1>
+            <div className="container mx-auto px-4 py-8 flex flex-wrap flex-col gap-6">
+                <Link className="bg-white flex flex-col p-10 gap-6" href="portfolio/relynolli">
+                    <Image
+                        className="dark:invert"
+                        src="relynolli/file.svg"
+                        alt="Relynolli"
+                        width={30}
+                        height={38}
+                    />
+                    <div className="flex flex-col p-2">
+                        <h3 className="font-bold text-3xl text-background">Relynolli</h3>
+                        <p className="font-medium text-xl text-background">Проект, с которым не получилось
+                            подружиться</p>
+                    </div>
+                </Link>
+                <Link className="bg-white flex flex-col p-10" href="portfolio/relynolli">
+                    <Image
+                        className="dark:invert"
+                        src="relynolli/file.svg"
+                        alt="Relynolli"
+                        width={30}
+                        height={38}
+                    />
+                    <h3 className="font-bold text-3xl text-background">Relynolli</h3>
+                    <p className="font-medium text-xl text-background">Проект, с которым не получилось подружиться</p>
+                </Link>
             </div>
         </div>
     );
